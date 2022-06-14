faded626f816eeaf4e2fe30afb3c1fa9a1cc46ce
lawrencejews <lawrencejews1@gmail.com>
2022-06-14 15:03:06 +0900

    added UI, components

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+
+  [SerializeField] Transform m_target;
+  [SerializeField] Vector3 m_boardCenter;
+  Vector3 m_offset;
+
+  void Start()
+  {
+    // Calculate the offset at start
+    m_offset = transform.position - m_boardCenter;
+  }
+  void Update()
+  {
+    // Set the movement target to the target's position plus an X and Z offset
+    Vector3 target = new Vector3(m_target.position.x + m_offset.x,
+                                transform.position.y,
+                                m_target.position.z + m_offset.z);
+
+    // Linearly interpolate the position from the current position to the target
+    transform.position = Vector3.Lerp(transform.position,
+    target,
+    Time.deltaTime);
+  }
+}
